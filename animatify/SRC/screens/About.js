25922982dc4a0df41f50ca6d6f1c054b3b5130dc
Matboud <m.matboud@gmail.com>
2020-08-16 19:23:28 +0100

    handling navigation

---
+++ animatify/SRC/screens/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class About extends Component {
+   render() {
+      return (
+         <View>
+            <Text> About </Text>
+         </View>
+      )
+   }
+}
+
+const styles = StyleSheet.create({})
---
+++ animatify/SRC/screens/Saved.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class Saved extends Component {
+   render() {
+      return (
+         <View>
+            <Text> Saved </Text>
+         </View>
+      )
+   }
+}
+
+const styles = StyleSheet.create({})
