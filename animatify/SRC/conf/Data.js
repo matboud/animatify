bd795345620c2c943e4f2bcd111e986464d26b84
Matboud <m.matboud@gmail.com>
2020-08-21 17:33:07 +0100

    animations data

---
+++ animatify/SRC/conf/Data.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Animation1 from '../animations/Animation1'
+
+export const Data = [
+   {
+      id: '0',
+      component: <Animation1 />,
+   },
+   {
+      id: '0w',
+      component: <Animation1 />,
+   },
+   {
+      id: '0wf',
+      component: <Animation1 />,
+   },
+];
