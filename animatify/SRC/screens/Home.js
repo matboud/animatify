aff90fa818fb4cb0b464cf841f7978620f1c81bc
Matboud <m.matboud@gmail.com>
2020-08-21 17:32:13 +0100

    how data ll be rendered in the Home component

--- animatify/SRC/screens/Home.js
+++ animatify/SRC/screens/Home.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native';
+import {
+   SafeAreaView,
+   Text,
+   StyleSheet,
+   View,
+   FlatList
+} from 'react-native';
+
+// IMporting components
+import Card from '../components/Card';
+
+const Data = require('../conf/Data');
 
 export default class Home extends Component {
    render() {
-      return (
-         <View>
-            <Text> Home </Text>
+
+      const renderItem = ({ Animation }) => (
+         <View style={styles.renderedItem}>
+            <Card child={Animation} />
          </View>
+      );
+
+      console.log(Data.Data)
+      return (
+         <SafeAreaView style={styles.safeView}>
+            <FlatList
+               data={Data.Data}
+               renderItem={renderItem}
+               keyExtractor={item => item.id}
+               contentContainerStyle={styles.flatListContainer}
+            />
+         </SafeAreaView>
       )
    }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+   safeView: {
+      flex: 1,
+      backgroundColor: '#ecf0f1'
+   },
+   renderedItem: {
+      paddingHorizontal: 15,
+      marginBottom: 20,
+   },
+   flatListContainer: {
+      paddingTop: 30,
+      paddingBottom: 10
+   }
+})
