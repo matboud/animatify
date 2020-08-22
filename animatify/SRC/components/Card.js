0229b0468bfc65968d5465cac7930fb8e9fd7bfd
Matboud <m.matboud@gmail.com>
2020-08-22 19:34:57 +0100

    setting up the card environment

--- animatify/SRC/components/Card.js
+++ animatify/SRC/components/Card.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native';
+
+// icons
+import Heart_i_filled from '../assets/svg/Heart_i_filled';
+import Heart_i from '../assets/svg/Heart_i';
+import Code_i from '../assets/svg/Code_i';
 
 export default function Card({ child }) {
    return (
       <View style={styles.container}>
          <View style={styles.header}>
-            <Text>test</Text>
-            <Text>test</Text>
+            <View style={styles.headerLeft}>
+               <Text style={styles.headerTitle}>title</Text>
+            </View>
+
+            <View style={styles.headerRight}>
+               <View style={styles.headerIconHolder}>
+                  <Code_i width={22} height={22}/>
+               </View>
+               <View style={styles.headerIconHolder}>
+                  {
+                     // <Heart_i_filled width={20} height={20}/>
+                     <Heart_i width={20} height={20} />
+                  }
+               </View>
+            </View>
          </View>
          <View >
             {
@@ -45,5 +63,25 @@ const styles = StyleSheet.create({
       backgroundColor: '#f9f9f9',
       borderBottomWidth: 0.3,
       borderBottomColor: '#a4b0be'
+   },
+   headerLeft: {
+      flex: 2,
+      // backgroundColor: 'yellow'
+   },
+   headerRight: {
+      flex: 1,
+      flexDirection: 'row',
+      // backgroundColor: 'red',
+      justifyContent: 'flex-end',
+      alignItems: 'center'
+   },
+   headerTitle: {
+      fontSize: 18,
+      color: '#2f3542'
+   },
+   headerIconHolder: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingLeft: 30
    }
 })
